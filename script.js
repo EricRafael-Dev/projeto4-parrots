a2e2fa83f3e1c58d3ef450ec6cabfb9d313b0363
Eric Rafael <rafaelpsec@gmail.com>
2023-04-09 12:29:07 -0300

    'Build deck of cards' implemented

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 let amount_cards = prompt('Com quantas cartas gostaria de jogar? (Escolha um número par entre 4 a 14)')
 let amount_cards_int = Number(amount_cards)
 let amount_okay = 'false'
+let jogadas = 0
+let clock_count = 0
+
+
+
+
+let firstCardFront;
+let firstCardBack;
+let firstCardSelected;
+let classFirstCard;
+
+let secondCardFront;
+let secondCardBack;
+let secondCardSelected;
+let classSecondCard;
 
 
 
@@ -35,5 +50,50 @@ while (amount_okay == 'false'){
     }
 }
 
+
+
+function build_deck(amount_cards_int){
+
+    array = ['bobrossparrot.gif',
+    'explodyparrot.gif',
+    'fiestaparrot.gif',
+    'metalparrot.gif',
+    'revertitparrot.gif',
+    'tripletsparrot.gif',
+    'unicornparrot.gif']
+
+    const list = array.sort(()=> Math.random() - 0.5);
+
+    var deck = document.querySelector(".cards")
+    
+
+    let array_combined =[]
+    
+    for(let i=0; i<=(amount_cards_int/2)-1; i++){
+
+        //Adiciona os pares em relação a quantidade de cartas escolhidas
+
+        array_combined.push(list[i])
+        array_combined.push(list[i])
+    }
+
+    const list_combined = array_combined.sort(()=> Math.random() - 0.5);
+
+    for(let j=0; j<list_combined.length; j++){
+        
+        //Mostra a quantidade de cartas embaralhadas
+
+        deck.innerHTML += `<div onclick="virarCarta(this)" class="card">
+            <div class="card_front face">
+              <img src="./Assets/back.png">
+            </div>
+            <div class="${list_combined[j].slice(0,(list_combined[j].length)-4)} card_back back-face face" id=''>
+              <img src="./Assets/Deck/${list_combined[j]}">
+            </div>
+        </div>`
+    }
+
+    
+}
 
 
